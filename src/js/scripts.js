41a3e3b589e4dbdeb27d60c9f0db1bec1405cb8b
Kutabix <bartekd123456@wp.pl>
2024-10-06 14:57:11 +0200

    koniec

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,4 @@
-import * as THREE from '../../node_modules/three/build/three.module.js';
-
+import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GUI } from 'dat.gui';  
 
